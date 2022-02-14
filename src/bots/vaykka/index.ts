099d805498ff8be7c40ffb13bf5b48d4ade74322
Teppo Jalava <tjjalava@iki.fi>
2022-02-14 11:55:17 +0200

    Case insensitive Väyrynen, who replies in threads also

--- src/bots/vaykka/index.ts
+++ src/bots/vaykka/index.ts
@@ -1,18 +1,23 @@
 import utteranceGenerator from '../../lib/utterances'
 import vaykka from './vaykka.json'
-import { App } from '@slack/bolt'
+import { App, GenericMessageEvent } from '@slack/bolt'
 import { BotRegistration } from '../index'
 
 const utterances = utteranceGenerator(vaykka)
 
 const register: BotRegistration = (app: App) => {
-  app.message(/vaykka|väykkä|paavo|väyry|väykä/, async ({ say }) => {
-    await say({
-      text: utterances.next(),
-      icon_emoji: ':vaykka:',
-      username: 'Väykkä'
-    }).catch(err => console.error(err))
-  })
+  app.message(
+    /vaykka|väykkä|paavo|pave|väyry|väykä/i,
+    async ({ say, message }) => {
+      const { thread_ts } = message as GenericMessageEvent
+      await say({
+        text: utterances.next(),
+        icon_emoji: ':vaykka:',
+        username: 'Väykkä',
+        thread_ts
+      }).catch(err => console.error(err))
+    }
+  )
 }
 
 export default register
